aef64700201eec96b7083649efb49f55c6b57a19
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-05-15 18:06:50 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    const body = document.querySelector("body");
+    notification.className = `notification ${type}`;
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    notification.innerHTML = `
+<h2 class="title">${title}</h2>
+<p>${description}</p>`;
+    body.prepend(notification);
+    setTimeout(()=>notification.style.visibility = "hidden", 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.<br> Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.<br> Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.<br> Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
